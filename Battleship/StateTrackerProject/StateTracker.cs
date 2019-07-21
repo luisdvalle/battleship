a2e00dc5317ae8c7290ffdd45b4cc6fa30678b7c
Luis Del Valle <luis@luis-delvalle.com>
2019-07-22 00:49:18 +1000

    Adding useless (so far) wrapper class for Game model

---
+++ Battleship/StateTrackerProject/StateTracker.cs
@@ -0,0 +1,35 @@
+﻿using StateTrackerProject.Domain;
+using StateTrackerProject.Interfaces;
+
+namespace StateTrackerProject
+{
+    public class StateTracker
+    {
+        private readonly IGame _game;
+
+        public StateTracker(string name)
+        {
+            _game = new Game(name);
+        }
+
+        public void StartGame()
+        {
+            _game.ActivateGame();
+        }
+
+        public bool AddShipToBoard(char x, int y, int length, char shipOrientation)
+        {
+            return _game.AddShipToBoard(x, y, length, shipOrientation);
+        }
+
+        public bool AssessAttack(char x, int y)
+        {
+            return _game.AttackCoordinate(x, y);
+        }
+
+        public bool HasGameEnded()
+        {
+            return _game.ActiveShips() < 1;
+        }
+    }
+}
