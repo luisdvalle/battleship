a42005906fe53b00cd0256f516857a99b4d50aa7
Luis Del Valle <luis@luis-delvalle.com>
2019-07-22 00:41:01 +1000

    Adding initial UTs for Game functionality

---
+++ Battleship/StateTrackerTests/GameTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace StateTrackerTests
+{
+    public class GameTests
+    {
+        [Fact]
+        public void CreateGame_WhenValidPlayerAndBoardGiven_ReturnsActiveGame()
+        {
+            var game = new Game("John Doe");
+
+            game.ActivateGame();
+
+            Assert.True(game.Active);
+        }
+    }
+}
