b222f68cb69ac85dc9a9f0a149620edd1b04eb58
Luis Del Valle <luis@luis-delvalle.com>
2019-07-22 00:16:53 +1000

    Adding initial UTs for Ship model

---
+++ Battleship/StateTrackerTests/ShipTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace StateTrackerTests
+{
+    public class ShipTests
+    {
+        [Fact]
+        public void TakeHit_WhenValidCoordinateGiven_SetsCoordinateAsHit()
+        {
+            var coordinates = new List<Coordinate>
+            {
+                new Coordinate(0, 1),
+                new Coordinate(0, 2),
+                new Coordinate(0, 3)
+            };
+            var ship = new Ship(coordinates);
+
+            ship.TakeHit(new Coordinate(0, 1));
+
+            Assert.Equal(CoordinateState.Hit, ship.Coordinates[new Coordinate(0, 1)]);
+        }
+    }
+}
