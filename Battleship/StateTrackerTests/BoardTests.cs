4b00515cde050279e9bdd463ea7d7bf3e93f5a9d
Luis Del Valle <luis@luis-delvalle.com>
2019-07-22 00:30:20 +1000

    Adding first UTs for Board functionality

---
+++ Battleship/StateTrackerTests/BoardTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace StateTrackerTests
+{
+    public class BoardTests
+    {
+        [Fact]
+        public void ActivateBoard_WhenValidInputIsGiven_ReturnsActiveBoard()
+        {
+            var board = new Board();
+
+            board.ActivateBoard();
+
+            Assert.Equal(ModelState.Active, board.State);
+            Assert.Equal(100, board.Coordinates.Length);
+        }
+    }
+}
